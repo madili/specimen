4ddcf5653f569e0a8bbf8498e0c55e21d8fbec6d
thiagomajesk <thiagomgoulart@gmail.com>
2021-08-09 12:51:54 -0300

    test: add factory tests

--- test/specimen/factory_test.exs
+++ test/specimen/factory_test.exs
@@ -2,6 +2,7 @@ defmodule Specimen.FactoryTest do
   use ExUnit.Case, async: true
 
   alias UserFixture, as: User
+  alias UserFixtureFactory, as: Factory
 
   defmodule OtherModule, do: defstruct([:name])
   defmodule EmptyFactory, do: use(Specimen.Factory, User)
@@ -12,5 +13,14 @@ defmodule Specimen.FactoryTest do
     message = "This factory can't be used to build Specimen.FactoryTest.OtherModule"
 
     assert_raise RuntimeError, message, fn -> EmptyFactory.build(specimen) end
+  end
+
+  test "make_one/1 is exposed in the factory" do
+    assert %User{name: "Joe", lastname: "Schmoe", status: "active"} = Factory.make_one([:status])
+  end
+
+  test "make_many/2 is exposed in the factory" do
+    assert [%User{name: "Joe", lastname: "Schmoe", status: "active"}] =
+             Factory.make_many(1, [:status])
   end
 end
